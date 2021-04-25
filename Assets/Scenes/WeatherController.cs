45f19720cc43727ecd6cada5b0e079fdc9f98fce
jedsteward <44725946+jedsteward@users.noreply.github.com>
2021-04-25 14:14:45 +0100

    isDayTime setup - never daytime

--- Assets/Scenes/WeatherController.cs
+++ Assets/Scenes/WeatherController.cs
@@ -8,15 +8,17 @@ using System.IO;
 public class WeatherController : MonoBehaviour
 {
     private const string API_KEY = "4176e58953a958e5c778f1db8803f6e7";
-    public string CityId;
-    public GameObject SnowSystem;
-    public GameObject SunSystem;
+    public string cityId;
+    public GameObject snowSystem;
+    public GameObject sunSystem;
+    private WeatherInfo currentWeather;
 
     // Start is called before the first frame update
     void Start()
     {
+        currentWeather = GetWeather();
         CheckSnowStatus();
-
+        checkDayTimeStatus();
     }
 
     // Update is called once per frame
@@ -25,32 +27,46 @@ public class WeatherController : MonoBehaviour
 
     }
 
-    public void CheckSnowStatus()
+    private void CheckSnowStatus()
     {
-        WeatherInfo currentWeather = GetWeather();
-        Debug.Log(String.Format("Got weather for {0}, is {1}", CityId, currentWeather.weather[0].main));
+        Debug.Log(String.Format("Got weather for {0}, is {1}", cityId, currentWeather.weather[0].main));
         bool snowing = currentWeather.weather[0].main.Equals("Snow");
         if (snowing)
         {
-            SnowSystem.SetActive(true);
-            SunSystem.SetActive(false);
+            snowSystem.SetActive(true);
         }
         else
         {
-            SnowSystem.SetActive(false);
-            SunSystem.SetActive(true);
+            snowSystem.SetActive(false);
+        }
+    }
+    private void checkDayTimeStatus()
+    {
+        if (isDayTime())
+        {
+            sunSystem.SetActive(true);
+        }
+        else
+        {
+            sunSystem.SetActive(false);
         }
     }
 
     private WeatherInfo GetWeather()
     {
-        string url = String.Format("http://api.openweathermap.org/data/2.5/weather?id={0}&APPID={1}", CityId, API_KEY);
+        string url = String.Format("http://api.openweathermap.org/data/2.5/weather?id={0}&APPID={1}", cityId, API_KEY);
         HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
         HttpWebResponse response = (HttpWebResponse)request.GetResponse();
         StreamReader reader = new StreamReader(response.GetResponseStream());
         string jsonResponse = reader.ReadToEnd();
         WeatherInfo info = JsonUtility.FromJson<WeatherInfo>(jsonResponse);
         return info;
+    }
+
+    private bool isDayTime()
+    {
+        // do stuff
+        return false;
     }
 
 
